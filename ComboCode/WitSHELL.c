b62e655ea55927a3e78a2e24434b7b6314a07e40
kapitanspider <34584378+kapitanspider@users.noreply.github.com>
2019-01-20 13:33:07 +0100

    Update WitSHELL.c

--- ComboCode/WitSHELL.c
+++ ComboCode/WitSHELL.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <dirent.h>
 		int pid;
 		char command[50];
 		char *p, *a;
@@ -29,9 +30,8 @@ int main()
 		getcwd(path, sizeof(path));
 		gethostname(host, sizeof(host));
 		printf("\n");
-		printf("%s",host);
-		printf(" ");
-		printf("%s",path);
+		printf("%s%s@%s",GREEN,getenv("USER"),host);
+		printf("%s%s%s",BLUE,path,NORM);
 		printf("$");
 		fgets(command, sizeof(command),stdin);	
 		if(strcmp(command,"exit\n")==0)	 
@@ -41,9 +41,9 @@ int main()
 		else if(strcmp(command,"help\n")==0)
 		{
 			printf("%s***************************************************\n",GREEN);
-			printf("%s** Autor: Witold Borowiak                        **\n",GREEN);
+			printf("%s** %sAutor: Witold Borowiak%s                        **\n",GREEN,CYAN,GREEN);
 			printf("%s***************************************************\n",GREEN);
-			printf("%s** Lista komend: exit, help, cp a b ,            **\n",GREEN);
+			printf("%s** Lista komend: exit, help, cp, touch, cd       **\n",GREEN);
 			printf("%s***************************************************\n",GREEN);
 			printf("%s",NORM);
 			
@@ -67,7 +67,7 @@ int main()
 				FILE *source, *target;
 				if(args[1]==NULL)
 				{
-					printf("Nie podano pliku zrodlowego");
+					printf("%sBlad: %sNie podano pliku zrodlowego%s",RED,YELLOW,NORM);
 				}
 				else
 				{
@@ -75,7 +75,7 @@ int main()
  						source = fopen(args[1], "r");
 						if(args[2]==NULL)
 						{
-							printf("Nie podano pliku docelowego");
+							printf("%sBlad: %sNie podano pliku docelowego%s",RED,YELLOW,NORM);
 						}	
 						else
 						{
@@ -90,13 +90,54 @@ int main()
  					}
  					else
  						{
- 							printf("Nie zrodlowy nie istnieje");
+ 							printf("%sBlad: %sNie zrodlowy nie istnieje%s",RED,YELLOW,NORM);
 						}
  				}
 			}
+			else if(strcmp(a,"touch")==0)
+			{
+				if(args[1]==NULL)
+				{
+					printf("%sBlad: %sNie podano nazwy pliku%s",RED,YELLOW,NORM);
+				}
+				else
+				{
+					FILE *target;
+					target=fopen(args[1], "w");
+					fclose(target);
+
+				}
+				
+			}
+			else if(strcmp(a,"whoami")==0)
+			{
+				printf("%s\n", getenv("USER"));
+			}
+			else if(strcmp(a,"cd")==0)
+			{
+				if(args[1]==NULL)
+				{
+					chdir("/home");
+				}
+				else
+				{
+					DIR* dir = opendir(args[1]);
+					if (dir)
+					{
+					chdir(args[1]);	
+    				closedir(dir);
+					}
+					else 
+					{
+						printf("%sBlad: %sKatalog nie istnieje%s",RED,YELLOW,NORM);
+					}
+					
+				}
+			}
 			else
 			{
-				pid = fork(); 	
+				printf("%sOperacja wykonana poza microshellem%s",MAGENTA,NORM);
+				pid = fork();
 				if(pid>0) wait(NULL);
 				if(pid==0)
 				{
