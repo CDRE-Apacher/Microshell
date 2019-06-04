6ce395e7285a4918f54a90eb9032b4cb5f639ea0
Mark <cdreapacher@gmail.com>
2019-06-04 15:15:41 +0200

    Progress

--- Prog_ob/V_renting/main.cpp
+++ Prog_ob/V_renting/main.cpp
@@ -13,7 +13,6 @@ MAKE ALL THE MANUAL FUNCTION CALLS
 using namespace std;
 
 /* --PERSON CLASSES-- */
-
 class Person{
 public:
 	string name;
@@ -48,7 +47,13 @@ public:
 
 	string to_string(){
 		stringstream ss;
-		ss<<"#"<<this->p_id<<":--:"<<this->name<<":--:"<<this->surname<<":--:"<<this->age<<":--:"<<this->position<<endl;
+		
+		ss<<"#"<<this->p_id
+		<<" :|: "<<this->name
+		<<" :|: "<<this->surname
+		<<" :|: "<<this->age
+		<<" :|: "<<this->position
+		<<endl;
 		string str = ss.str();
 		return str;
 	}
@@ -81,8 +86,6 @@ public:
 				}
 			}
 		}
-		
-		
 	}
 
 private:
@@ -90,35 +93,22 @@ private:
 	static int p_next_id;
 	static vector<Person*> people;
 };
-
 int Person::p_next_id = 1;
 vector<Person*> Person::people;
 
-
 class Employee : public Person{
-public:
-	
+public:	
 	Employee(string _name, string _surname, int _age, string _position):Person(_name, _surname, _age, _position){}
-
-	//~Employee();
-
 };
 
 class Customer : public Person{
 public:
 	Customer(string _name, string _surname, int _age):Person(_name, _surname, _age, "Customer"){}
-
-	//~Customer();
-
-private:
-
 };
 
 /* --VEHICLE CLASSES-- */
-
 class Vehicle{
 public:
-	
 	string producer;
 	string model;
 	int production_year;
@@ -136,10 +126,9 @@ public:
 	int hull_buoyancy;
 //AIR
 	string a_hull_type;
-	int lift; // in neutons
-	
+	int lift;
+//LEASING	
 	int is_leased = 0;
-
 	int leased_to = 0;
 
 	Vehicle(string _model, string _producer, int _production_year, int _num_passengers, char _fuel_type, int _fuel_cap, int _weight, char _type, int _doors, int _trunk_cap, string _w_hull_type, int _hull_buoyancy, string _a_hull_type, int _lift, int _is_leased){
@@ -198,7 +187,25 @@ public:
 
 	string to_string(){
 		stringstream ss;
-		ss<<"#"<<this->v_id<<":--:"<<this->model<<":--:"<<this->producer<<":--:"<<this->production_year<<":--:"<<this->num_passengers<<":--:"<<this->fuel_type<<":--:"<<this->fuel_cap<<":--:"<<this->weight<<":--:"<<this->type<<":--:"<<this->doors<<":--:"<<this->trunk_cap<<":--:"<<this->w_hull_type<<":--:"<<this->hull_buoyancy<<":--:"<<this->a_hull_type<<":--:"<<this->lift<<":--:"<<this->is_leased<<":--:"<<this->is_leased<<endl;
+		
+		ss<<"#"<<this->v_id
+		<<" :|: "<<this->model
+		<<" :|: "<<this->producer
+		<<" :|: "<<this->production_year
+		<<" :|: "<<this->num_passengers
+		<<" :|: "<<this->fuel_type
+		<<" :|: "<<this->fuel_cap
+		<<" :|: "<<this->weight
+		<<" :|: "<<this->type
+		<<" :|: "<<this->doors
+		<<" :|: "<<this->trunk_cap
+		<<" :|: "<<this->w_hull_type
+		<<" :|: "<<this->hull_buoyancy
+		<<" :|: "<<this->a_hull_type
+		<<" :|: "<<this->lift
+		<<" :|: "<<this->is_leased
+		<<endl;
+		
 		string str = ss.str();
 		return str;
 	}
@@ -267,65 +274,62 @@ public:
 /* --MAIN-- */
 int main(){
 	new Land("Astra","Opel",2010,5,'D',40,2500,5,350);
-	new Water("D-2500","Mitsubichi",2012,2,'G',20,120,"FIBER",190);
-	new Air("C-23","Airwing",2000,20,'N',2500,4000,"ALU/FIBER",6800);
-	
-	new Land("2 Astra","Opel",2010,5,'D',40,2500,5,350);
-	new Water("2 D-2500","Mitsubichi",2012,2,'G',20,120,"FIBER",190);
-	new Air("2 C-23","Airwing",2000,20,'N',2500,4000,"ALU/FIBER",6800);
-	
-	Vehicle::del_from_vector(3);
-	
-	vector<Vehicle*> a = Vehicle::get_object_vector();
-	
-	for(Vehicle* i : a){
-		cout<<i->to_string()<<endl;
-	}
-	
-	new Employee("Wojtek", "Niedzwiedz", 43, "Salesman");
-	new Customer("Pawel", "Jumper", 23);
-	
-	vector<Person*> b = Person::get_object_vector();
-	
-	for(Person* j : b){
-		cout<<j->to_string()<<endl;
-	}
-	
-	Vehicle::leasing(2,2);
-	
-	a = Vehicle::get_object_vector();
-	
-	for(Vehicle* i : a){
-		cout<<i->to_string()<<endl;
-	}
-	
-	Vehicle::leasing(2,2);
-	
-	Vehicle::return_leasing(2);
-	
-	Vehicle::leasing(2,1);
-	
-	a = Vehicle::get_object_vector();
-	
-	for(Vehicle* i : a){
-		cout<<i->to_string()<<endl;
-	}
+	new Land("Panda","Fiat",2007,7,'G',70,2000,5,500);
+	new Land("Juke","Nissan",2000,5,'G',65,1900,5,440);
+	new Land("Focus","Ford",1999,5,'G',45,2300,3,300);
+
+	new Water("Safari 28","Carter Marine",2012,6,'G',240,1200,"FIBER",1700);
+	new Water("Safari 56","Carter Marine",2002,8,'G',290,1400,"FIBER",1900);
+	new Water("Kirk","Amel Yachts",2016,12,'G',350,2500,"FIBER/METAL",3000);
+	new Water("Fango","Amel Yachts",2008,16,'G',370,2700,"METAL/WOOD",3500);
+
+	new Air("Everett-747","Boeing",2000,120,'N',5000,4000,"ALU/FIBER",9500);
+	new Air("A-380","Airbus",20080,240,'N',5500,4500,"ALU/FIBER",12000);
+	new Air("C-5 Galaxy","Lockheed Martin",2016,20,'N',65000,6000,"ALU",10000);
+	new Air("GE9X","GE Aviation",2006,2,'N',2500,2500,"FIBER",6800);
+
 	new Employee("Staszek","Grzmot",33,"Mechanic");
-	
-	
+	new Employee("Wojtek", "Daminski", 43, "Salesman");
+	new Employee("Andrzej", "Stachowiak", 28, "Deliveryman");
+	new Employee("Patryk", "Wolny", 46, "Mechanic");
+	new Employee("Zbigniew", "Knur", 39, "Mechanic");
+
+	new Customer("Marek", "Wolski", 23);
+	new Customer("Witold", "Brzeski", 34);
+	new Customer("Damian", "Makowski", 36);
+	new Customer("Adrian", "Woltycki", 54);
+	/*
+	Vehicle::del_from_vector(3);
+	vector<Vehicle*> a = Vehicle::get_object_vector();
+		for(Vehicle* i : a){
+			cout<<i->to_string()<<endl;
+		}
+	vector<Person*> b = Person::get_object_vector();
+		for(Person* j : b){
+			cout<<j->to_string()<<endl;
+		}
+	Vehicle::leasing(2,2);
+	a = Vehicle::get_object_vector();
+		for(Vehicle* i : a){
+			cout<<i->to_string()<<endl;
+		}
+	Vehicle::leasing(2,2);
+	Vehicle::return_leasing(2);
+	Vehicle::leasing(2,1);
+	a = Vehicle::get_object_vector();
+		for(Vehicle* i : a){
+			cout<<i->to_string()<<endl;
+		}
 	b = Person::get_object_vector();
-	for(Person* j : b){
-		cout<<j->to_string()<<endl;
-	}
-	
-	
+		for(Person* j : b){
+			cout<<j->to_string()<<endl;
+		}
 	Person::del_from_vector(2);
+		b = Person::get_object_vector();
+		for(Person* j : b){
+			cout<<j->to_string()<<endl;
+		}
+	*/
 	
-	b = Person::get_object_vector();
-	for(Person* j : b){
-		cout<<j->to_string()<<endl;
-	}
-	
-	
-	return 0;
+	return EXIT_SUCCESS;
 }
